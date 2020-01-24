6017276bc32c1dbf4fa74b6774bee34016d924dc
es <StaNC2000>
2020-01-24 13:49:31 +0100

    Init for method getGreatestCommonDivisor

--- src/com/eS/GreatestCommonDivisor.java
+++ src/com/eS/GreatestCommonDivisor.java
@@ -2,6 +2,10 @@ package com.eS;
 
 public class GreatestCommonDivisor {
 
+    public static int getGreatestCommonDivisor(int first, int second){
+
+    }
+
     public static void main(String[] args) {
 	// write your code here
     }
