d6ec18ccfbe9a7c2cfdb0982faee852741100a03
es <StaNC2000>
2020-01-24 14:21:16 +0100

    Method getGreatestCommonDivisor created using while loop  and tested

--- src/com/eS/GreatestCommonDivisor.java
+++ src/com/eS/GreatestCommonDivisor.java
@@ -2,11 +2,29 @@ package com.eS;
 
 public class GreatestCommonDivisor {
 
-    public static int getGreatestCommonDivisor(int first, int second){
+    public static int getGreatestCommonDivisor(int firstNumber, int secondNumber){
 
+        int commonDivider = 0;
+
+        if (firstNumber < 10 || secondNumber < 10){
+            return - 1;
+        }
+
+        while (firstNumber != secondNumber){
+            if (firstNumber > secondNumber){
+                firstNumber = firstNumber - secondNumber;
+            }
+            else {
+                secondNumber = secondNumber - firstNumber;
+            }
+        }
+        commonDivider = firstNumber;
+
+        return commonDivider;
     }
-
     public static void main(String[] args) {
 	// write your code here
+
+        System.out.println(getGreatestCommonDivisor(12, 30));
     }
 }
