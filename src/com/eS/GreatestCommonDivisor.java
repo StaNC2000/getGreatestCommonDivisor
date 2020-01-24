899d41571823c1cc5de5c5bbdfdef7c30ee67f69
es <StaNC2000>
2020-01-24 14:46:12 +0100

    Method getGreatestCommonDivisor while loop commented out, created for loop version

--- src/com/eS/GreatestCommonDivisor.java
+++ src/com/eS/GreatestCommonDivisor.java
@@ -2,26 +2,44 @@ package com.eS;
 
 public class GreatestCommonDivisor {
 
-    public static int getGreatestCommonDivisor(int firstNumber, int secondNumber){
+    public static int getGreatestCommonDivisor(int firstNumber, int secondNumber) {
 
-        int commonDivider = 0;
+        int commonDivider = 1;
 
         if (firstNumber < 10 || secondNumber < 10){
             return - 1;
         }
+//
+//        while (firstNumber != secondNumber){
+//            if (firstNumber > secondNumber){
+//                firstNumber = firstNumber - secondNumber;
+//            }
+//            else {
+//                secondNumber = secondNumber - firstNumber;
+//            }
+//        }
+//        commonDivider = firstNumber;
+//
+//        return commonDivider;
 
-        while (firstNumber != secondNumber){
-            if (firstNumber > secondNumber){
-                firstNumber = firstNumber - secondNumber;
+        for (int i = 1; i < firstNumber && i <= secondNumber; i++) {
+            System.out.println(i + " " + firstNumber + " " + secondNumber);
+            if (firstNumber % i == 0 && secondNumber % i == 0) {
+                System.out.println("First " + firstNumber + " Second " + secondNumber + " Divider " + i);
+                commonDivider = i;
             }
+
             else {
-                secondNumber = secondNumber - firstNumber;
-            }
+
+                System.out.println("First " + firstNumber + " Second " + secondNumber + " No divide by " + i);            }
+
+
         }
-        commonDivider = firstNumber;
 
         return commonDivider;
+
     }
+
     public static void main(String[] args) {
 	// write your code here
 
